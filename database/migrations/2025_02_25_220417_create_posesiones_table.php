f99a22d3487da2f63d63dbcbdabf44d5eed66484
José Antonio Dominguez <joseantonio.dominguez@iesdonana.org>
2025-02-26 14:23:19 +0100

    Softdeletes donde faltaban

--- database/migrations/2025_02_25_220417_create_posesiones_table.php
+++ database/migrations/2025_02_25_220417_create_posesiones_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('videojuego_id')->constrained();
             $table->foreignId('user_id')->constrained();
             $table->unique(['videojuego_id', 'user_id']);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
